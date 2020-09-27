5bf5d945396c397ca9614e54ad9ee5a0dcd066c4
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2020-09-27 18:04:20 -0500

    Animation implemented.

--- timer.js
+++ timer.js
@@ -16,11 +16,11 @@ class Timer {
 
   start = () => {
     if (this.onStart) {
-      this.onStart();
+      this.onStart(this.timeRemaining);
     }
 
     this.tick();
-    this.interval = setInterval(this.tick, 1000);
+    this.interval = setInterval(this.tick, 25);
   };
 
   pause = () => {
@@ -35,10 +35,10 @@ class Timer {
         this.onComplete();
       }
     } else {
-      this.timeRemaining = this.timeRemaining - 1;
+      this.timeRemaining = this.timeRemaining - 0.025;
 
       if (this.onTick) {
-        this.onTick();
+        this.onTick(this.timeRemaining);
       }
     }
   };
@@ -48,6 +48,6 @@ class Timer {
   }
 
   set timeRemaining(time) {
-    this.durationInput.value = time;
+    this.durationInput.value = time.toFixed(2);
   }
 }
